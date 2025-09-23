7f550c73436eb98f098aaa9e1bf47c3d89d7d54f
Travis Hummel <travisnhummel@gmail.com>
2025-09-23 12:14:35 +1000

    Initial spring boot backend setup

---
+++ src/test/java/com/noisemap/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noisemap.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
